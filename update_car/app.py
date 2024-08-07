333c5dd6b1c8dfbd4e60cd4775426f7f307a4846
Lisseth Fuentes <105747977+AlixStart313@users.noreply.github.com>
2024-08-06 18:22:33 -0600

    Update app.py

--- update_car/app.py
+++ update_car/app.py
@@ -17,7 +17,7 @@ def lambda_handler(event, __):
     try:
         body = json.loads(event.get('body', '{}'))
 
-        if not 'brand' in body or not 'model' in body or not 'power' in body or not 'weigth' in body or not 'id' in body:
+        if not 'brand' in body or not 'model' in body or not 'year' in body or not 'color' in body or not 'id' in body:
             return {
                 "statusCode": 400,
                 "headers": headers,
@@ -29,8 +29,8 @@ def lambda_handler(event, __):
         id = body.get('id')
         brand = body.get('brand')
         model = body.get('model')
-        power = body.get('power')
-        weigth = body.get('weigth')
+        power = body.get('year')
+        weigth = body.get('color')
 
         if not brand or not model or not power or not weigth or not id:
             return {
@@ -114,7 +114,7 @@ def update_motosierra (id, brand, model, power, weigth):
     try:
         connection = connect_to_database()
         cursor = connection.cursor()
-        cursor.execute("UPDATE Motosierras SET brand = %s, model = %s, power = %s, weigth = %s WHERE id = %s", (brand, model, power, weigth, id))
+        cursor.execute("UPDATE Motosierras SET brand = %s, model = %s, power = %s, weight = %s WHERE id = %s", (brand, model, power, weigth, id))
         connection.commit()
         connection.close()
         return True
